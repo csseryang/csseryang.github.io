7501fa9ff38b89285aea867c71c9862adbeef961
Hui(Philip) Yang <csseryang@gmail.com>
2016-05-28 16:33:59 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,150 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+      <!--<link rel="icon" href="../../favicon.ico">-->
+
+    <title>Welcome!</title>
+
+    <!-- Bootstrap core CSS -->
+      <link href="http://getbootstrap.com/assets/css/docs.min.css" rel="stylesheet">
+    <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="http://getbootstrap.com/assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+     <div class="page-header">
+        <h1>This will be a better website!</h1>
+      </div>
+        <div class = "row">
+          <div class = "col-md-4">
+
+
+          <img data-src="holder.js/200x200" class="img-thumbnail" alt="200x200" src="https://scontent.fsjc1-2.fna.fbcdn.net/hphotos-xpa1/t31.0-8/10506966_1454535701486702_7434460290360822118_o.jpg" data-holder-rendered="true" style="width: 200px; height: 200px;">
+          </div>
+        
+            <div class = "col-md-6">
+                <p>Hi! I'm Hui Yang, a M.S student in computer science at University of California, Riverside. I am from <a href="http://en.wikipedia.org/wiki/Shenzhen_University">Shenzhen University</a>
+                    <br><br>I grow up in <a href="http://en.wikipedia.org/wiki/Shenzhen">Shenzhen</a>, which is next to Hong Kong. <br> <br>
+
+My area of interests are computer networks and distributed systems. I am looking for jobs in network/software engineering. <br> <br>
+
+                  
+                <strong>Email</strong>: csseryang [at] gmail [dot] com</p>  
+                [<a href="https://drive.google.com/file/d/0B428rXBxUzFeQXJwR3NmT01fTDA/view?usp=sharing">cv</a>] [ <a href="https://www.linkedin.com/in/hui-yang-3b5482a1
+">Linkedin</a>]
+            </div>
+            
+        </div>
+         <hr>
+        <div class="page-header">
+        <h3>Research Experience</h3>
+        </div>
+       
+          <div><strong>
+      Live Video Content Distribution Study(on going) </strong>    <div class="pull-right">   September 2015 - Present</br></div>
+      </div>
+      <p><br>Research topic at UCR under the direction of Professor
+
+      <a href="http://www.cs.ucr.edu/~jiasi/">Jiasi Chen</a>  <br>
+
+<ul>
+<li>Conduct extensive measurements to study live video architectures like Twitch/Skype/Google handout to figure out how to reduce end-to-end latency and improve user experience.</li>
+<li>Implement the core algorithm in Matlab.</li>
+        </p>
+</ul>
+         <div class="page-header">
+        <h3>Project Experience</h3>
+        </div>
+         
+            
+              
+         
+            <div><strong>
+   C#-Based Video Game  </strong>[<a href="https://www.youtube.com/watch?v=M7S1sDhXUX4">video</a>]    <div class="pull-right">   March 2015 - June 2015</br></div>
+      </div>
+           <br>
+          <div>
+                    <p> <strong>The Factory </strong> is a first person 3D survival game developed with Unity in c#. It is a abet accredited senior design project with Kevin Chang
+              , Patrick Ly-Vo and Jai Kakkar. Music by Jordan Hang.(CS 179)
+                     <br>
+                        <ul>
+                          <li> The player must collect certain items to beat the level and different items have different functions. <br>
+                        
+                           <li> Maze is randomly generated using Minimum Spanning Tree Algorithm.<br>
+                           <li> The enemies’ AI utilizes A* pathfinding, navigating arbitrarily around the maze, and chasing the player when seen.<br>
+                        </ul>
+                    </p> 
+           </div>
+          
+       
+        
+               <div><strong>
+  C++-Based Tool for Bioengineering  </strong>[<a href="http://i.imgur.com/hu0gxd2.gifv">image</a>]    <div class="pull-right">   January 2015 - June 2015</br></div>
+  </div>
+             <br>
+
+             <div>
+           
+          
+
+                   <p> <strong>Fret Analyzer </strong>is a computational tool developed for a biogneering professor at UCR.
+                        It takes in experimental fluorescent data,
+                        use the data to calculate useful parameters and display the results in a graphical way.
+                        The program is developed with QT 5.4, we use C++ to implement the core algorithm and Python
+                        to do data visualization.
+                    
+                        <br>
+
+                        
+                        
+                </p>   
+               
+        
+       </div>
+         
+      
+        
+        <div class="page-header">
+        <h3>Others</h3>
+            <hr>
+            <p> <strong>Running and Hiking</strong>--- <a href="http://baike.baidu.com/view/3319683.htm">Shenzhen Annual 100km hiking</a><br>
+                <strong>Reading</strong>--- Chinese History, especially <a href="https://en.wikipedia.org/wiki/Ming_dynasty">Ming Dynaste </a><br>
+                       <strong>Traveling</strong>  
+                </p>  
+            
+            
+        </div>
+        
+      <footer class="footer">
+        <p>&copy; Hui Yang 2015. Powered by Bootstrap and SAE</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
